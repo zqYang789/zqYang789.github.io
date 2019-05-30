050c1755425e793c81af85560acbd72b4a2fa4fd
yangzq@sunsan.com.cn <zqYang789>
2019-05-30 09:02:43 +0800

    修改

--- demo/birthday/js/indexl.js
+++ demo/birthday/js/indexl.js
@@ -25,7 +25,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Hi brother|祝你|生日快乐|happy birthday|#countdown 3||');
+      S.UI.simulate('Hi friend|祝你|生日快乐|happy birthday|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
